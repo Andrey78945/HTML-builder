3f810169639fa19c68b20b7b9e67024a8c6e71eb
Andrei <platonovaudit@yandex.ru>
2022-11-05 19:56:57 +0500

    feat: add if_error to subtask 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 const path = require("path");
 const dirPath = path.join(__dirname, "./secret-folder/");
 
-console.log(dirPath);
-
 fs.readdir(dirPath, (err, files) => {
+    if (err) {
+        throw new Error(err);
+    }
     files.forEach(file => {
         fs.stat(path.join(dirPath, file), (err, stats) => {
             if (err) {
-                console.error(err)
-                return
+                throw new Error(err);
             }
             if (stats.isFile()) console.log(file, `${stats.size}Kb`)
         })
