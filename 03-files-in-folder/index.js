5e248c0f6c340295c2dd215cf57529eee923562d
Andrei <platonovaudit@yandex.ru>
2022-11-05 20:19:42 +0500

    fix: output to subtask 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -11,7 +11,13 @@ fs.readdir(dirPath, (err, files) => {
             if (err) {
                 throw new Error(err);
             }
-            if (stats.isFile()) console.log(file, `${stats.size}Kb`)
+            if (stats.isFile()) {
+                let ext = path.extname(file);
+                let name = path.basename(file);
+                let index = name.indexOf('.')
+                name = index === -1 ? name : name.slice(0, index);
+                console.log(name, ext.slice(1), `${stats.size / 1024}Kb`)
+            }
         })
     });
 });
